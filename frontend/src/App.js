0bbbb49d702c7c30c72319734f16242b0218100d
Hritik <hritiksolanki699@gmail.com>
2023-07-18 20:42:57 +0530

    inisial frontend done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
+import { Fragment } from "react";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <Fragment>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </Fragment>
+  );
 }
 
 export default App;
