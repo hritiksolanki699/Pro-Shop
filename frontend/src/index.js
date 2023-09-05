cbf14fe2cf2bc9ae6cbaef21ba190723d515cf8e
Hritik <hritiksolanki699@gmail.com>
2023-09-05 21:20:02 +0530

    checkout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,11 +13,14 @@ import { Provider } from "react-redux";
 import store from "./store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import PrivateRoute from './components/PrivateRoute';
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,7 +30,13 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
+         {/* Registered users */}
+         <Route path='' element={<PrivateRoute />}>
+        <Route path='/shipping' element={<ShippingScreen />} />
+        <Route path='/payment' element={<PaymentScreen />} />
+      </Route>
     </Route>
+    
   )
 );
 
