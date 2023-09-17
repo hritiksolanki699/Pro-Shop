804cf3e0dab731574e7b9af9bab68ed922913340
Hritik <hritiksolanki699@gmail.com>
2023-09-17 20:27:35 +0530

    deployment first version

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,26 @@
+import path from 'path';
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import {
-  notFound,
-  errorHandler
-} from "./middleware/errorMiddleware.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
-connectDB(); // connect to the database
+connectDB();
 
 const app = express();
 
 // Body parser middleware
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // Cookie Parser middleware
 app.use(cookieParser());
@@ -29,13 +29,29 @@ app.get("/", (req, res) => {
   res.send("API is running.....");
 });
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
-app.get('/api/config/paypal', (req, res) => res.send({
-  clientId: process.env.PAYPAL_CLIENT_ID
-}));
+app.get("/api/config/paypal", (req, res) =>
+  res.send({
+    clientId: process.env.PAYPAL_CLIENT_ID,
+  })
+);
+
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
